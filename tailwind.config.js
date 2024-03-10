4324f4e89ef3bb05530b1dbc7f20c373e5856074
Mike <marko.v.polo.solo@gmail.com>
2024-03-10 21:02:17 +0300

    included icons in navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+        colors:{
+          pastelBlue:{
+            500: '#525E75'
+          },
+          pastelGreen:{
+            200: '#92BA92',
+            500: '#78938A'
+          },
+          pastelYellow:{
+            300: '#F1DDBF'
+          },
+        }
+    },
   },
   plugins: [],
 }
